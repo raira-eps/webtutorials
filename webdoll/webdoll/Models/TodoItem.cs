ea2c85c66c56cd59c95e8ced57ef320763c7bf04
raira-eps <anhappynorthwind39@ezweb.ne.jp>
2020-01-14 01:12:42 +0900

    first commit

---
+++ webdoll/webdoll/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webdoll.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; } //idプロパティは、RDB内の一意のキーとして機能する
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ webdoll/webdoll/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore; //add
+using webdoll.Models;                //add
+
+namespace webdoll
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // このメソッドはRuntimeによって呼び出されます。このメソッドを使用して、コンテナにサービスを追加します。
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // このメソッドはRuntimeによって呼び出されます。 このメソッドを使用して、HTTPrequestパイプラインを構成します。
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
